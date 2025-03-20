f30af4ccc7e725954c1f312a09bcbe666a04d0a5
Ciaran Cumiskey <ciaran.cumiskey@zinkworks.com>
2025-03-20 15:52:52 +0000

    Added example of few-shot prompting.

--- gemini_services/gemini_client.py
+++ gemini_services/gemini_client.py
@@ -6,7 +6,7 @@ def send_text_prompt(api_key: str, prompt_text: str) -> str:
     # TODO: implement sending text prompt to Gemini API
     # You can use the provided endpoint and request method as a reference
     client = genai.Client(api_key=api_key)
-    print(f"Sending prompt {prompt_text} to Gemini")
+    print(f"Sending prompt \"{prompt_text}\" to Gemini")
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=prompt_text
--- gemini_services/gemini_test.py
+++ gemini_services/gemini_test.py
@@ -12,18 +12,33 @@ if __name__ == '__main__':
     try:
         text_prompt = sys.argv[1]
     except IndexError:
-        print("Text prompt was not supplied, switching to default.")
-        text_prompt = ("You are an experienced mechanic. I have bought a car, and the brake pedal goes soft when I "
-                       "turn the ignition key. What are the likely causes of this?")
+        print("Text prompt was not supplied, quitting.")
+        sys.exit(1)
     api_key = ""
     try:
         api_key = sys.argv[2]
     except IndexError:
         print("Defaulting to API key saved to environment.")
         api_key = os.environ.get("gcpTrainingApiKey")
-        if api_key.strip() == '':
+        if api_key is None or api_key.strip() == '':
             print("No API key found in environment or command line arguments.")
             sys.exit(1)
     gemini_response_text = send_text_prompt(api_key, text_prompt)
     print(gemini_response_text)
-    # TODO: Experiment with zero-shot, few-shot and chain-of-thought prompting
+    # Experiment with few-shot prompting
+    text_prompt = ("You are an experienced graphic designer. Please provide a list of hexadecimal colour codes for a "
+                   "particular theme in a JSON list. Do NOT include any comments or explanations for your choices, "
+                   "just a list like '[#hexCode1, #hexCode2, #hexCode3]'. "
+                   "Your responses must follow the following format:\n"
+                   "Theme: Clay and Sea\n"
+                   "['#C7522A', '#E5C185', '#FBF2C4', '#74A892', '#008585']\n"
+                   "Theme: Pastel Vibes\n"
+                   "['#D6E6FF', '#D7F9F8', '#FFFFEA', '#FFF0D4', '#FBE0E0', '#E5D4EF']\n"
+                   "Theme: Moody Sunset\n"
+                   "['#003F5C', '#58508D', '#BC5090', '#FF6361', '#FFA600']\n"
+                   "Theme: Forest Breeze\n"
+                   "['#F1DDBF', '#525E75', '#78938A', '#92BA92']\n"
+                   "Theme: Calsonic Skyline\n")
+    gemini_response_text = send_text_prompt(api_key, text_prompt)
+    print(gemini_response_text)
+    # TODO: chain-of-thought prompting
