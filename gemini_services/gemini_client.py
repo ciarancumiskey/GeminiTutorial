a2f75b3d5f824fd6fc62b4df0f450fb3d1be9604
Ciaran Cumiskey <ciaran.cumiskey@zinkworks.com>
2025-03-24 11:06:25 +0000

    Created gemini_client.send_n_shot_prompt() for N-shot prompting.
    Moved Gemini Client creation to gemini_test.py.
    
    Added examples of chain-of-thought prompting.

--- gemini_services/gemini_client.py
+++ gemini_services/gemini_client.py
@@ -1,14 +1,40 @@
 from google import genai
-import requests
+from google.genai import client, types
 
 
-def send_text_prompt(api_key: str, prompt_text: str) -> str:
-    # TODO: implement sending text prompt to Gemini API
-    # You can use the provided endpoint and request method as a reference
-    client = genai.Client(api_key=api_key)
+def send_text_prompt(gemini_client: client, model: str, prompt_text: str) -> str:
     print(f"Sending prompt \"{prompt_text}\" to Gemini")
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
+
+    response = gemini_client.models.generate_content(
+        model=model,
         contents=prompt_text
     )
+    return response.text
+
+
+def send_n_shot_prompt(gemini_client: client, model: str, system_instructions: str, chat_history: list[dict],
+                       new_user_message: str) -> str:
+
+    print("Sending N-shot prompts to Gemini.")
+
+    contents = []
+    for sample_chat in chat_history:
+        contents.append(genai.types.Content(
+            parts=[{'text': sample_chat.get("chat")}],
+            role=sample_chat.get("role")
+        ))
+    print(contents)
+    generate_content_config = types.GenerateContentConfig(
+        temperature=1,
+        top_p=0.95,
+        top_k=40,
+        max_output_tokens=8192,
+        system_instruction=system_instructions
+    )
+
+    new_chat = gemini_client.chats.create(
+            model=model,
+            config=generate_content_config,
+            history=contents)
+    response = new_chat.send_message(new_user_message)
     return response.text
--- gemini_services/gemini_test.py
+++ gemini_services/gemini_test.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-from gemini_client import send_text_prompt
+from google import genai
+
+from gemini_client import send_text_prompt, send_n_shot_prompt
 
 # TODO: Get API key
 # TODO: Hide API key, maybe make it a CLI param
@@ -23,22 +25,38 @@ if __name__ == '__main__':
         if api_key is None or api_key.strip() == '':
             print("No API key found in environment or command line arguments.")
             sys.exit(1)
-    gemini_response_text = send_text_prompt(api_key, text_prompt)
+    client = genai.Client(api_key=api_key)
+    # Default to lite as the model
+    model = "gemini-2.0-flash-lite"
+    gemini_response_text = send_text_prompt(client, model, text_prompt)
     print(gemini_response_text)
     # Experiment with few-shot prompting
-    text_prompt = ("You are an experienced graphic designer. Please provide a list of hexadecimal colour codes for a "
-                   "particular theme in a JSON list. Do NOT include any comments or explanations for your choices, "
-                   "just a list like '[#hexCode1, #hexCode2, #hexCode3]'. "
-                   "Your responses must follow the following format:\n"
-                   "Theme: Clay and Sea\n"
-                   "['#C7522A', '#E5C185', '#FBF2C4', '#74A892', '#008585']\n"
-                   "Theme: Pastel Vibes\n"
-                   "['#D6E6FF', '#D7F9F8', '#FFFFEA', '#FFF0D4', '#FBE0E0', '#E5D4EF']\n"
-                   "Theme: Moody Sunset\n"
-                   "['#003F5C', '#58508D', '#BC5090', '#FF6361', '#FFA600']\n"
-                   "Theme: Forest Breeze\n"
-                   "['#F1DDBF', '#525E75', '#78938A', '#92BA92']\n"
-                   "Theme: Calsonic Skyline\n")
-    gemini_response_text = send_text_prompt(api_key, text_prompt)
+    n_shots = [
+        dict(role="user", chat="Theme: Clay and Sea"),
+        dict(role="model", chat="['#C7522A', '#E5C185', '#FBF2C4', '#74A892', '#008585']"),
+        dict(role="user", chat="Theme: Pastel Vibes"),
+        dict(role="model", chat="['#D6E6FF', '#D7F9F8', '#FFFFEA', '#FFF0D4', '#FBE0E0', '#E5D4EF']"),
+        dict(role="user", chat="Theme: Moody Sunset"),
+        dict(role="model", chat="['#003F5C', '#58508D', '#BC5090', '#FF6361', '#FFA600']"),
+        dict(role="user", chat="Theme: Forest Breeze"),
+        dict(role="model", chat="['#F1DDBF', '#525E75', '#78938A', '#92BA92']")
+    ]
+    palette_instructions = (
+        "You are an experienced graphic designer. Please provide a list of hexadecimal colour codes for a "
+        "particular theme in a JSON list. Do NOT include any comments or explanations for your choices, "
+        "just a list like '[#hexCode1, #hexCode2, #hexCode3]'. ")
+    gemini_response_text = send_n_shot_prompt(client, model, palette_instructions, n_shots, "Theme: Calsonic Skyline")
     print(gemini_response_text)
-    # TODO: chain-of-thought prompting
+    # Chain-of-thought prompting examples
+    text_prompt = ("In the 2024 Champions League final, Real Madrid extended their record of Champions League titles"
+                   "to 15 after defeating Borussia Dortmund. How were they successful? Please explain step by "
+                   "step.")
+    gemini_response_text = send_text_prompt(client, model, text_prompt)
+    print(gemini_response_text)
+    text_prompt = ("In the 2024 Formula One season, Max Verstappen won a fourth drivers' championship in a row. How "
+                   "was he successful? Please explain step by step.")
+    gemini_response_text = send_text_prompt(client, model, text_prompt)
+    print(gemini_response_text)
+    text_prompt = "What is the best strategy of Fantasy Premier League? Please explain step by step."
+    gemini_response_text = send_text_prompt(client, model, text_prompt)
+    print(gemini_response_text)
